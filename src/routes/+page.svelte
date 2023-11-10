c5b0ea85ef2c0df10f557826e323b2c1e35f0f73
Josh <jshbker@gmail.com>
2023-11-10 20:26:40 +0000

    test server functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-
+    import type { PageData } from "./$types";
+	export let data: PageData
 </script>
 
 <svelte:head>
 	<title>Demo Application</title>
 </svelte:head>
 
-<h1>Welcome</h1>
+<h1>Welcome to {data.host}</h1>
---
+++ src/routes/api/+server.ts
@@ -0,0 +1,4 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types'
+
+export const GET: RequestHandler = () => json({ hello: "world" })
